793218d733c9067e19a2577b7c0cbb344684ce4a
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-10-10 21:07:46 +0000

    changed the private key. Hopefully this works

--- api/github/webhooks/index.js
+++ api/github/webhooks/index.js
@@ -6,8 +6,11 @@ const { createNodeMiddleware, createProbot } = require("probot");
 const app = require("../../../app");
 const probot = createProbot({
   appId: process.env.APP_ID,
-  privateKey: process.env.PRIVATE_KEY,
+  privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
   secret: process.env.WEBHOOK_SECRET
 });
 
 module.exports = createNodeMiddleware(app, {probot, webhooksPath: "/api/github/webhooks"});
+
+// if it doesn't work, try this: Import reply to issue and events from app.js. 
+// do probot.on(events, replyToIssue) in this file
