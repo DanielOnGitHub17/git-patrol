cfafc1a4e0919ee262a1f6f8496514ec22768627
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-10-10 19:59:26 +0000

    Refactor webhooks index.js file

--- api/github/webhooks/index.js
+++ api/github/webhooks/index.js
@@ -1,17 +1,17 @@
-// // api/github/webhooks/index.js
-// import { createNodeMiddleware, createProbot } from "probot";
+// api/github/webhooks/index.js
+import { createNodeMiddleware, createProbot } from "probot";
 
-// import app from "../../../app.js";
-// console.log("hi");
+import app from "../../../app.js";
+console.log("hi");
 
-// export default createNodeMiddleware(app, {
-//   probot: createProbot(),
-//   webhooksPath: "/api/github/webhooks",
-// });
+export default createNodeMiddleware(app, {
+  probot: createProbot(),
+  webhooksPath: "/api/github/webhooks",
+});
 
-const { createNodeMiddleware, createProbot } = require("probot");
+// const { createNodeMiddleware, createProbot } = require("probot");
 
-const app = require("../../../app");
-const probot = createProbot();
+// const app = require("../../../app");
+// const probot = createProbot();
 
-module.exports = createNodeMiddleware(app, { probot, webhooksPath: '/api/github/webhooks' });
+// module.exports = createNodeMiddleware(app, { probot, webhooksPath: '/api/github/webhooks' });
